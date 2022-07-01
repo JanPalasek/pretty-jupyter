26c8637a32e8ecbfdb22bada53e136ebb4ef80eb
Jan Palasek <26582151+JanPalasek@users.noreply.github.com>
2022-07-01 20:35:33 +0200

    Code folding (#14)
    * Issue #12 - added option to code folding (from Rmd) with buttons, refactored TOC, allowed to turn it off.
    
    * Issue #12 - fixed ipykernel for this dependencies.

--- src/pretty_jupyter/templates/pj/static/pj.js
+++ src/pretty_jupyter/templates/pj/static/pj.js
@@ -43,6 +43,89 @@ $(document).ready(function () {
 
 });
 
+window.initializeCodeFolding = function(show) {
+  $("#jup-show-all-code").click(function() {
+    $('div.py-code-collapse').each(function() {
+      $(this).collapse('show');
+    });
+  });
+  $("#jup-hide-all-code").click(function() {
+    $('div.py-code-collapse').each(function() {
+      $(this).collapse('hide');
+    });
+  });
+
+  // index for unique code element ids
+  var currentIndex = 1;
+
+  // select all jupyter code blocks
+  var jupyterCodeBlocks = $('div.hl-ipython3');
+  jupyterCodeBlocks.each(function() {
+
+    // create a collapsable div to wrap the code in
+    var div = $('<div class="collapse py-code-collapse"></div>');
+    var showThis = (show || $(this).hasClass('fold-show')) && !$(this).hasClass('fold-hide');
+    if (showThis) div.addClass('in');
+    var id = 'code-643E0F36' + currentIndex++;
+    div.attr('id', id);
+    $(this).before(div);
+    $(this).detach().appendTo(div);
+
+    // add a show code button right above
+    var showCodeText = $('<span>' + (showThis ? 'Hide' : 'Code') + '</span>');
+    var showCodeButton = $('<button type="button" class="btn btn-default btn-xs code-folding-btn pull-right"></button>');
+    showCodeButton.append(showCodeText);
+    showCodeButton
+        .attr('data-toggle', 'collapse')
+        .attr('data-target', '#' + id)
+        .attr('aria-expanded', showThis)
+        .attr('aria-controls', id);
+
+    var buttonRow = $('<div class="row"></div>');
+    var buttonCol = $('<div class="col-md-12"></div>');
+
+    buttonCol.append(showCodeButton);
+    buttonRow.append(buttonCol);
+
+    div.before(buttonRow);
+
+    // update state of button on show/hide
+    div.on('hidden.bs.collapse', function () {
+      showCodeText.text('Code');
+    });
+    div.on('show.bs.collapse', function () {
+      showCodeText.text('Hide');
+    });
+  });
+}
+
+window.initializeTOC = function() {
+  // temporarily add toc-ignore selector to headers for the consistency with Pandoc
+  $('.unlisted.unnumbered').addClass('toc-ignore')
+
+  // move toc-ignore selectors from section div to header
+  $('div.section.toc-ignore')
+    .removeClass('toc-ignore')
+    .children('h1,h2,h3,h4,h5').addClass('toc-ignore');
+
+  // establish options
+  var options = {
+    selectors: "h1,h2,h3",
+    theme: "bootstrap3",
+    context: '.toc-content',
+    hashGenerator: function (text) {
+      return text.replace(/[.\\/?&!#<>]/g, '').replace(/\s/g, '_');
+    },
+    ignoreSelector: ".toc-ignore",
+    scrollTo: 0
+  };
+  options.showAndHide = true;
+  options.smoothScroll = true;
+
+  // tocify
+  var toc = $("#TOC").tocify(options).data("toc-tocify");
+}
+
 
 if (window.hljs) {
     hljs.configure({
@@ -71,8 +154,6 @@ $(document).ready(function () {
 
 
 // tabsets 
-
-
 $(document).ready(function () {
   window.buildTabsets("TOC");
 });
@@ -82,39 +163,6 @@ $(document).ready(function () {
     $(this).parent().toggleClass('nav-tabs-open')
   });
 });
-
-
-// code folding 
-
-$(document).ready(function () {
-
-  // temporarily add toc-ignore selector to headers for the consistency with Pandoc
-  $('.unlisted.unnumbered').addClass('toc-ignore')
-
-  // move toc-ignore selectors from section div to header
-  $('div.section.toc-ignore')
-    .removeClass('toc-ignore')
-    .children('h1,h2,h3,h4,h5').addClass('toc-ignore');
-
-  // establish options
-  var options = {
-    selectors: "h1,h2,h3",
-    theme: "bootstrap3",
-    context: '.toc-content',
-    hashGenerator: function (text) {
-      return text.replace(/[.\\/?&!#<>]/g, '').replace(/\s/g, '_');
-    },
-    ignoreSelector: ".toc-ignore",
-    scrollTo: 0
-  };
-  options.showAndHide = true;
-  options.smoothScroll = true;
-
-  // tocify
-  var toc = $("#TOC").tocify(options).data("toc-tocify");
-});
-
-
 
 
 // dynamically load mathjax for compatibility with self-contained 
