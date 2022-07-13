dc930f164f7d690f29cc26f1f542ac2f81a056a6
JanPalasek <jan.palasek@seznam.cz>
2022-07-13 21:53:52 +0200

    Issue #29 - added selenium test.

---
+++ tests/conftest.py
@@ -0,0 +1,31 @@
+import pytest
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.core.utils import ChromeType
+import selenium.webdriver
+
+import chromedriver_binary
+
+@pytest.fixture()
+def driver():
+    chrome_service = Service(ChromeDriverManager(chrome_type=ChromeType.GOOGLE).install())
+
+    chrome_options = Options()
+    options = [
+        "--headless",
+        "--disable-gpu",
+        "--window-size=1920,1080",
+        "--ignore-certificate-errors",
+        "--disable-extensions",
+        "--no-sandbox",
+        "--disable-dev-shm-usage"
+    ]
+    for option in options:
+        chrome_options.add_argument(option)
+
+    driver = selenium.webdriver.Chrome(service=chrome_service, options=chrome_options)
+
+    yield driver
+
+    driver.close()
