72c8283fbaa4b6b03f4535e174e9a0e52601e947
JanPalasek <jan.palasek@seznam.cz>
2022-08-11 16:32:28 +0200

    Issue #50 - moved metadata to subattribute, added behaviour with meta.

--- src/pretty_jupyter/preprocessors/_metadata_preprocessor.py
+++ src/pretty_jupyter/preprocessors/_metadata_preprocessor.py
@@ -16,30 +16,32 @@ class NbMetadataPreprocessor(Preprocessor):
 
     defaults = {
         "title": "Untitled",
-        "html": {
-            "toc": True,
-            "toc_depth": 3,
-            "toc_collapsed": True,
-            "toc_smooth_scroll": True,
-            "number_sections": False,
-            "code_folding": "hide",
-            "theme": "paper",
-            "include_plotlyjs": True
+        "output": {
+            "output_stream_stderr": False,
+            "output_stream_stdout": True,
+            "input_jinja_markdown": False,
+            "html": {
+                "toc": True,
+                "toc_depth": 3,
+                "toc_collapsed": True,
+                "toc_smooth_scroll": True,
+                "number_sections": False,
+                "code_folding": "hide",
+                "theme": "paper",
+                "include_plotlyjs": True
+            }
         },
-        "output_stream_stderr": False,
-        "output_stream_stdout": True,
-        "input_jinja_markdown": False,
     }
 
-    overrides = Dict(default_value={})
+    pj_metadata = Dict(default_value={})
     """
     Dictionary with all values to override the defaults. Note that per_key_trait is not an exhaustive list, you can define your own new override.
     """
 
     def __init__(self, **kw):
-        if "overrides" in kw and isinstance(kw["overrides"], str):
+        if "pj_metadata" in kw and isinstance(kw["pj_metadata"], str):
             # convert to dictionary
-            kw["overrides"] = ast.literal_eval(kw["overrides"])
+            kw["pj_metadata"] = ast.literal_eval(kw["pj_metadata"])
 
         super().__init__(**kw)
 
@@ -49,10 +51,10 @@ class NbMetadataPreprocessor(Preprocessor):
         # 1. Values specified by user in NbMetadataProcessor.overrides
         # 2. Values specified by user in notebook metadata
         # 3. Default values from NbMetadataProcessor.defaults
-        metadata = merge_dict(self.overrides, nb.metadata.copy())
+        metadata = merge_dict(self.pj_metadata, nb.metadata.get("pj", {}).copy())
         metadata = merge_dict(metadata, self.defaults)
 
-        resources["nb_metadata"] = metadata
+        resources["pj_metadata"] = metadata
 
         return super().preprocess(nb, resources)
 
@@ -63,20 +65,20 @@ class NbMetadataPreprocessor(Preprocessor):
         if cell.cell_type != "code":
             return cell, resources
 
-        metadata = resources["nb_metadata"]
+        output_metadata = resources["pj_metadata"]["output"]
 
         # process jinja markdown
-        if not metadata["input_jinja_markdown"] and is_jinja_cell(cell.source):
+        if not output_metadata["input_jinja_markdown"] and is_jinja_cell(cell.source):
             cell.transient = {"remove_source": True}
 
         # process stderr, stdout
         if len(cell.outputs) > 0:
             for i, output in enumerate(cell.outputs):
                 # stderr
-                if not metadata["output_stream_stderr"] and output.output_type == "stream" and output.name == "stderr":
+                if not output_metadata["output_stream_stderr"] and output.output_type == "stream" and output.name == "stderr":
                     cell.outputs.pop(i)
                 # stdout
-                elif not metadata["output_stream_stdout"] and output.output_type == "stream" and output.name == "stdout":
+                elif not output_metadata["output_stream_stdout"] and output.output_type == "stream" and output.name == "stdout":
                     cell.outputs.pop(i)
         
         return cell, resources
