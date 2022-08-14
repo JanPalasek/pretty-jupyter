9aa291c0fd670de70a9210b9c96da01ba00246fd
Jan Palasek <26582151+JanPalasek@users.noreply.github.com>
2022-08-14 15:42:08 +0200

    Refactored. (#55)
    * Refactored.
    
    * Opravy v js.
    
    * Fixed setup.cfg.
    
    * Fixed missing libraries because gitignore.
    
    * Fix

--- src/pretty_jupyter/templates/pj/static/pj.js
+++ src/pretty_jupyter/templates/pj/static/pj.js
@@ -5,6 +5,7 @@
 $.fn.nextUntilWithTextNodes = function (until) {
   var matched = $.map(this, function (elem, i, until) {
     var matched = [];
+
     while ((elem = elem.nextSibling) && elem.nodeType !== 9) {
       if (elem.nodeType === 1 || elem.nodeType === 3) {
         if (until && jQuery(elem).is(until)) {
