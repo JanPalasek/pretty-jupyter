3d6ef4ac74f348bc609ae07b8bc12db950df17ad
Jan Palasek <26582151+JanPalasek@users.noreply.github.com>
2023-07-08 12:09:20 +0200

    Technical upgrades (#70)
    * Update of the packages.
    
    * Update of the packages.
    
    * Update chrome service.
    
    * Fixed tests.
    
    * Switched mainly to pyproject.toml, added pre-commit.
    
    * Cleanup of not used files, fix of pre-commit.

---
+++ make.ps1
@@ -0,0 +1,55 @@
+param([string]$command)
+
+$venv = $env:VENV
+if ([string]::IsNullOrEmpty($venv)) {
+    $venv = "venv"
+}
+
+function Activate-Venv([string]$venv) {
+    python -m venv "$venv"
+    Write-Host "Activating environment '$venv'"
+    & ./$venv/Scripts/Activate.ps1
+}
+
+function Pip-Tools-Compile {
+    Write-Host "Compile  packages into 'requirements-win.txt'..."
+    python -m piptools compile --extra=dev --resolver=backtracking --no-emit-index-url -o requirements-win.txt pyproject.toml
+}
+
+function Pip-Tools-Sync {
+    Write-Host "Syncing packages in 'requirements-win.txt' with the activated environment..."
+    python -m piptools sync "requirements-win.txt"
+    python -m pip install -e .
+}
+
+
+switch ($command) {
+    "install_dev" {
+        Activate-Venv -venv $venv
+        python -m pip install -r requirements-win.txt
+        python -m pip install -e .
+        # python -m pre_commit install
+        break
+    }
+    "compile" {
+        Activate-Venv -venv $venv
+        Pip-Tools-Compile
+        break
+    }
+    "sync" {
+        Activate-Venv -venv $venv
+        Pip-Tools-Sync
+        break
+    }
+    "up" {
+        Activate-Venv -venv $venv
+        Pip-Tools-Compile
+        Pip-Tools-Sync
+        break
+    }
+    "compile-docs" {
+        Activate-Venv -venv $venv
+        Write-Host "Compile packages into 'docs/requirements-docs-win.txt'..."
+        python -m piptools compile --extra=docs --resolver=backtracking --no-emit-index-url -o docs/requirements-docs.txt pyproject.toml
+    }
+}
